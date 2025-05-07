d344e99f15d869e3b51b48854d326128ec0ae27f
Tomusdolny <tomasz.gorny@student.uj.edu.pl>
2025-05-07 14:24:56 +0200

    nic nowego

--- core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBackground.java
+++ core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBackground.java
@@ -9,7 +9,7 @@ public class MyBackground extends Actor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         Pixmap pixmap = new Pixmap(MyGame.WIDTH, MyGame.HEIGHT, Pixmap.Format.RGBA8888);
-        pixmap.setColor(0.6f, 0.3f, 0.1f, 1);
+        pixmap.setColor(0.6f, 0.3f, 0.1f, 1);   // brown color
 
         Texture brownBackground = new Texture(pixmap);
         batch.draw(brownBackground, 0,0);
