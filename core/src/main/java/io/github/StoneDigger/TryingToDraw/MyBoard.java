f8944af4eb19a7fb7cde79bc6021d1ac4e251c1c
Tomusdolny <tomasz.gorny@student.uj.edu.pl>
2025-05-07 16:43:50 +0200

    added TileType.getTexture(TileType) method; added board generating to MyBorad;

--- core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBoard.java
+++ core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBoard.java
@@ -1,38 +1,41 @@
 package io.github.StoneDigger.TryingToDraw;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import io.github.StoneDigger.BoardGenerators.Board;
+import io.github.StoneDigger.BoardGenerators.RandomBoardGenerator;
+import io.github.StoneDigger.BoardGenerators.TileType;
 
 import static io.github.StoneDigger.Assets.*;
 
 public class MyBoard extends Actor {
-    int[][] obstacleArray;
+    TileType[][] obstacleArray;
     int size_x;
     int size_y;
-    private final Texture[] textureIds = {
-        null,
-        DIRT_TEXTURE,
-        ROCK_TEXTURE,
-        DIAMOND_TEXTURE
-    };
+
     float sizeOfObstacleOnScreen = SIZE_TEXTURE + 20;
 
     public MyBoard() {
         size_x = 2;
         size_y = 2;
-        obstacleArray = new int[][]{{1,2}, {2,2}};
+        obstacleArray = new RandomBoardGenerator(0.1f,0.1f,0.1f).generate(30,20,1,1).getTiles();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
+        Color prev = batch.getColor();
+        batch.setColor(1f,1f,1f,1f);
+
         float temp = sizeOfObstacleOnScreen;
         float translate = (temp-SIZE_TEXTURE)/2;
         for(int i=0;i<size_x;i++) {
             for(int j=0;j<size_y;j++) {
-                batch.draw(textureIds[obstacleArray[i][j]], i*temp +translate, j*temp + translate, SIZE_TEXTURE, SIZE_TEXTURE);
+                batch.draw(TileType.getTexture(obstacleArray[i][j]), i*temp +translate, j*temp + translate, SIZE_TEXTURE, SIZE_TEXTURE);
             }
         }
+        batch.setColor(prev);
     }
 
     @Override
