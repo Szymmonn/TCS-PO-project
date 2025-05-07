c1ce90263e19b969b0f95ccf384fd41f093adc03
Tomusdolny <tomasz.gorny@student.uj.edu.pl>
2025-05-07 16:47:30 +0200

    fixed minor board generating issues in MyBoard

--- core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBoard.java
+++ core/src/main/java/io/github/StoneDigger/TryingToDraw/MyBoard.java
@@ -1,10 +1,8 @@
 package io.github.StoneDigger.TryingToDraw;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import io.github.StoneDigger.BoardGenerators.Board;
 import io.github.StoneDigger.BoardGenerators.RandomBoardGenerator;
 import io.github.StoneDigger.BoardGenerators.TileType;
 
@@ -12,15 +10,15 @@ import static io.github.StoneDigger.Assets.*;
 
 public class MyBoard extends Actor {
     TileType[][] obstacleArray;
-    int size_x;
-    int size_y;
+    int BoardSizeY;
+    int BoardSizeX;
 
     float sizeOfObstacleOnScreen = SIZE_TEXTURE + 20;
 
     public MyBoard() {
-        size_x = 2;
-        size_y = 2;
-        obstacleArray = new RandomBoardGenerator(0.1f,0.1f,0.1f).generate(30,20,1,1).getTiles();
+        BoardSizeX = 30;
+        BoardSizeY = 20;
+        obstacleArray = new RandomBoardGenerator(0.1f,0.1f,0.1f).generate(BoardSizeX,BoardSizeY,1,1).getTiles();
     }
 
     @Override
@@ -30,8 +28,8 @@ public class MyBoard extends Actor {
 
         float temp = sizeOfObstacleOnScreen;
         float translate = (temp-SIZE_TEXTURE)/2;
-        for(int i=0;i<size_x;i++) {
-            for(int j=0;j<size_y;j++) {
+        for(int i=0;i<BoardSizeX;i++) {
+            for(int j = 0; j< BoardSizeX; j++) {
                 batch.draw(TileType.getTexture(obstacleArray[i][j]), i*temp +translate, j*temp + translate, SIZE_TEXTURE, SIZE_TEXTURE);
             }
         }
