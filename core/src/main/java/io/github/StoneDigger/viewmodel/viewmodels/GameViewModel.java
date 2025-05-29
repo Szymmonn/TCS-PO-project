2b1f457de4eca24dc1d242a811ca62e797f5cadd
malinuus <kamil.1.adamczyk@student.uj.edu.pl>
2025-05-29 17:33:06 +0200

    gameViewModel fixed

--- core/src/main/java/io/github/StoneDigger/viewmodel/viewmodels/GameViewModel.java
+++ core/src/main/java/io/github/StoneDigger/viewmodel/viewmodels/GameViewModel.java
@@ -3,46 +3,51 @@ package io.github.StoneDigger.viewmodel.viewmodels;
 
 import io.github.StoneDigger.model.Classes.Board;
 import io.github.StoneDigger.model.Classes.GameLogic;
+import io.github.StoneDigger.model.Classes.Opponent;
 import io.github.StoneDigger.model.Classes.Player;
 import io.github.StoneDigger.model.Interfaces.EDirections;
-import io.github.StoneDigger.model1.models.BoardModel;
-import io.github.StoneDigger.model1.models.PlayerModel;
+
+import java.util.List;
 
 public class GameViewModel {
-    private GameLogic gameLogic;
     private Board board;
     private Player player;
+    private List<Opponent> opponentList;
 
-    public GameViewModel(Player playerModel, Board boardModel) {
-        gameLogic = new GameLogic();
-        this.player = playerModel;
-        this.board = boardModel;
+    public GameViewModel(Player player, Board board, List<Opponent> opponentList) {
+        GameLogic gameLogic = new GameLogic();
+        this.player = player;
+        this.board = board;
+        this.opponentList = opponentList;
     }
 
-    public Board getBoardModel() {
+    public Board getBoard() {
         return board;
     }
-
-    public Player getPlayerModel() {
+    public Player getPlayer() {
         return player;
     }
-
-    public void setBoardModel(Board boardModel) {
-        this.board = boardModel;
+    public List<Opponent> getOpponentList() {
+        return opponentList;
+    }
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    public void setOpponentList(List<Opponent> opponentList) {
+        this.opponentList = opponentList;
     }
 
-    public void setPlayerModel(Player playerModel) {
-        this.player = playerModel;
-    }
     public int getPlayerPositionX() {
         return player.getPosition().x;
     }
-
     public int getPlayerPositionY() {
         return player.getPosition().y;
     }
 
-    public void handleInput(EDirections dir) {
-
+    public void handleInput(EDirections direction) {
+        player.move(direction);
     }
 }
