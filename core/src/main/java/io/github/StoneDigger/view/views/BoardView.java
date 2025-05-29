19eb237f8f6986f1c09840e1038b7166c059537e
Tom <tomasz.gorny@student.uj.edu.pl>
2025-05-30 00:39:47 +0200

    added HUDView - left to find the right properties

--- core/src/main/java/io/github/StoneDigger/view/views/BoardView.java
+++ core/src/main/java/io/github/StoneDigger/view/views/BoardView.java
@@ -14,18 +14,9 @@ import static io.github.StoneDigger.view.screen.GameScreen.*;
 
 public class BoardView extends Actor {
     private final Board board;
-    private final Texture background;
 
     public BoardView(Board board) {
         this.board = board;
-
-        /// background initialization
-        Pixmap pix = new Pixmap(BOARD_WIDTH*(BLOCK_SIZE + GAP_SIZE), BOARD_HEIGHT*(BLOCK_SIZE + GAP_SIZE), Pixmap.Format.RGBA8888);
-        pix.setColor(0.6f, 0.3f, 0.1f, 1);
-        pix.fill();                           // <— actually paint
-        background = new Texture(pix);
-        pix.dispose();
-        ///
     }
 
     @Override
@@ -36,7 +27,6 @@ public class BoardView extends Actor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         Color prev = batch.getColor();
-        batch.draw(background, 0, 0, BOARD_WIDTH*(BLOCK_SIZE+GAP_SIZE), BOARD_HEIGHT*(BLOCK_SIZE+GAP_SIZE));
         batch.setColor(1,1,1,1);
         for(int i=0;i<BOARD_WIDTH;i++) {
             for(int j=0;j<BOARD_HEIGHT;j++) {
