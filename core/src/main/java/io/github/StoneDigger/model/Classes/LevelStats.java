ee0da9ae636c358fa9e502316066fea88d2f25ea
Szymmonn <szymon.chwila@student.uj.edu.pl>
2025-05-29 18:19:01 +0200

    modified gamestats interface and class

--- core/src/main/java/io/github/StoneDigger/model/Classes/LevelStats.java
+++ core/src/main/java/io/github/StoneDigger/model/Classes/LevelStats.java
@@ -2,13 +2,29 @@ package io.github.StoneDigger.model.Classes;
 
 import io.github.StoneDigger.model.Interfaces.ILevelStats;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class LevelStats implements ILevelStats {
     private int score;
+    private int HP;
+    private int levelNumber;
+    private LocalTime startTime;
+    public int getHP() {
+        return HP;
+    }
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+    public void setLevelNumber(int levelNumber) {
+        this.levelNumber = levelNumber;
+    }
+    public void setStartTime(LocalTime startTime) {this.startTime = startTime; }
+    public Duration timeElapsed() {return Duration.between(startTime, LocalTime.now()); }
     @Override public int getScore() { return score; }
     @Override public void addScore(int delta) { score += delta; }
-    @Override public boolean isLevelComplete() {
-        // Check if player has reached the exit position
-        // TODO: inject access to player and exit coordinates
-        return false;
-    }
 }
--- core/src/main/java/io/github/StoneDigger/model/Interfaces/ILevelStats.java
+++ core/src/main/java/io/github/StoneDigger/model/Interfaces/ILevelStats.java
@@ -3,6 +3,9 @@ package io.github.StoneDigger.model.Interfaces;
 public interface ILevelStats {
     int getScore();
     void addScore(int delta);
-    boolean isLevelComplete();
+    public int getLevelNumber();
+    void setLevelNumber(int levelNumber);
+    int getHP();
+    void setHP(int hp);
 }
 
