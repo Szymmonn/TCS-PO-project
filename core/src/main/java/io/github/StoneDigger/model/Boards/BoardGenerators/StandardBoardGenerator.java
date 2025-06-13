6b94bf691bc8c5a419d914ffe7f4222c544709fe
malinuus <kamil.1.adamczyk@student.uj.edu.pl>
2025-06-13 23:02:42 +0200

    my final version

--- core/src/main/java/io/github/StoneDigger/model/Boards/BoardGenerators/StandardBoardGenerator.java
+++ core/src/main/java/io/github/StoneDigger/model/Boards/BoardGenerators/StandardBoardGenerator.java
@@ -20,12 +20,9 @@ public class StandardBoardGenerator implements IBoardGenerator {
         start++;
         for(int y = 0; y < h; y ++) {
             for(int x = 0; x < w; x ++) {
-                if(start == w*h) System.out.println("XD");
                 level[y][x] = arr.charAt(start);
                 start ++;
-                System.out.print(level[y][x]);
             }
-            System.out.println();
         }
         return level;
     }
